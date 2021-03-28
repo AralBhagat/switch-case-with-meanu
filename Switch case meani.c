6845d6c9040d8126bdd4e19ad6ffc8152ae8bbfa
Aral <81504979+AralBhagat@users.noreply.github.com>
2021-03-28 14:07:50 +0530

    Create switch case meani.c
    On screen we came see a meanu and then we can switch it accordingly.

---
+++ Switch case meani.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	scanf("%d",&ch);
+	switch(ch){
+	
+	case 1: printf("pizza, RS.239");
+break;
+    case 2: printf("burger, RS.129");
+break;
+	case 3: printf("pasta, RS.179");
+            break;	
+	case 4: printf("freanch fries, RS.99");
+	        break;
+	case 5: printf("sandwitch, RS.149");	        
+}
+ return 0;
+}
